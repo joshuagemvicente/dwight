fba9c3a5779774bf83ce77612139d1e8887b5e71
joshuagemvicente <joshuagemvicente6@gmail.com>
2025-10-21 19:30:01 +0800

    betterAuth integration

---
+++ src/lib/auth.ts
@@ -0,0 +1,33 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { db } from "./db";
+
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECZreT } = process.env;
+
+export const auth = betterAuth({
+  database: prismaAdapter(db, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
+  // socialProviders: {
+  //   google: {
+  //     clientId: GOOGLE_CLIENT_ID || "",
+  //     clientSecret: GOOGLE_CLIENT_SECRET || "",
+  //   },
+  //   clientId: APPLE_CLIENT_ID || "",
+  //   apple: {
+  //     clientSecret: APPLE_CLIENT_SECRET || "",
+  //   },
+  // },
+});
+
+export const getUserSession = async (request: Request) => {
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
+
+  return session;
+};
