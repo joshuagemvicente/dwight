fee74113c0c7aeea6f8c3d15232e97d555cd65f9
joshuagemvicente <joshuagemvicente6@gmail.com>
2025-10-23 18:54:21 +0800

    Update socials component

--- src/components/shared/socials.tsx
+++ src/components/shared/socials.tsx
@@ -1,33 +1,38 @@
 import { Twitter, Instagram, Facebook } from "lucide-react";
+import Link from "next/link";
 
 interface SocialIcon {
   icon?: React.ReactNode;
+  href?: string;
 }
 
 const socialIcons: SocialIcon[] = [
   {
-    icon: <Twitter size={16} />,
+    icon: <Twitter size={24} />,
+    href: "#",
   },
   {
-    icon: <Instagram size={16} />,
+    icon: <Facebook size={24} />,
+    href: "#",
   },
   {
-    icon: <Facebook size={16} />,
+    icon: <Instagram size={24} />,
+    href: "#",
   },
 ];
 
 export function Socials() {
   return (
-    <div className="flex items-center gap-2 mt-5">
-      <p className="text-gray-400 text-xs">Socials |</p>
+    <div className="flex items-center mt-5">
       <div className="flex items-center gap-1">
         {socialIcons.map((social, index) => (
-          <div
+          <Link
+            href={social.href as string}
             className="text-gray-400 hover:text-black transition-all duration-300 hover:cursor-pointer"
             key={index}
           >
             {social.icon}
-          </div>
+          </Link>
         ))}
       </div>
     </div>
